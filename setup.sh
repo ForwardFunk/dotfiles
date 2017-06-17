c431393637136e06c7e6222f9ec4233a867137c7
Pavle Djordjevic <pavle.b.djordjevic@gmail.com>
2017-06-17 20:38:20 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-ln -s ~/dotfiles/.vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vim/vimrc ~/.vimrc && \
+ln -s ~/dotfiles/.vim ~/.vim && \
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
